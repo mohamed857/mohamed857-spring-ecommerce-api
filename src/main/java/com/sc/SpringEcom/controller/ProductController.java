7a7d423a7953e0f32216044c3486c81dd4dd1cd2
mohamed857 <mohamed857>
2025-01-07 15:41:28 +0200

    Initial commit

---
+++ src/main/java/com/sc/SpringEcom/controller/ProductController.java
@@ -0,0 +1,87 @@
+package com.sc.SpringEcom.controller;
+
+import com.sc.SpringEcom.model.Product;
+import com.sc.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("product/{productId}")
+    public ResponseEntity<?> getProductById(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        if (product.getId() < 0) {
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getProductImage(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        if (product.getId() >= 0) {
+            return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addOrUpdateProduct(product, image);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{productID}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        if (product.getId() >= 0) {
+            productService.deleteProduct(product);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/product")
+    public ResponseEntity<String> updateProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        Product updateProduct = null;
+        try {
+            updateProduct = productService.addOrUpdateProduct(product, image);
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("searching with :" + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/sc/SpringEcom/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.sc.SpringEcom.service;
+
+import com.sc.SpringEcom.model.Product;
+import com.sc.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return (Product) productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public void addProduct(Product product) {
+         productRepo.save(product);
+    }
+
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return (Product) productRepo.save(product);
+    }
+    public void deleteProduct(Product product){
+        productRepo.delete(product);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
+}
